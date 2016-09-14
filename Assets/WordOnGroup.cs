6e082d0f6361e287eecb2143ac4b4ae404da2bd1
MJ <Lin@mengs-mbp.mobile.asu.edu>
2016-09-14 02:37:59 -0700

    Add Menu Scene
    Add menu scene and make connection to the game scene.
    ps. attempting to build and run in mac.

--- Assets/WordOnGroup.cs
+++ Assets/WordOnGroup.cs
@@ -12,10 +12,12 @@ public class WordOnGroup : MonoBehaviour {
     // Use this for initialization
     void Start () {
         wordOnGroup = StartGameHandler.word;
-        char0 = wordOnGroup[0].ToString();
-        char1 = wordOnGroup[1].ToString();
-        char2 = wordOnGroup[2].ToString();
-        char3 = wordOnGroup[3].ToString();
+		if (wordOnGroup != string.Empty) {
+			char0 = wordOnGroup [0].ToString ();
+			char1 = wordOnGroup [1].ToString ();
+			char2 = wordOnGroup [2].ToString ();
+			char3 = wordOnGroup [3].ToString ();
+		}
     }
 	
 	// Update is called once per frame
